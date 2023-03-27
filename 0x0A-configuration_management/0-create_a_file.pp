877362768e4adbf059c0a7300783d0250f9577ef
Oladipo Mustapha <oladipomustapha001@gmail.com>
2023-03-27 02:12:19 -0700

    q1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to create a file in /tmp
+file { '/tmp/school':
+    ensure  => file,
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+}
